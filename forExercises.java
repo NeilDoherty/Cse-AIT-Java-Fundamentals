a1af57aebd162de54ccf87976071c166f74dd8b8
unknown <A00226313@W301-A16.studentnet.ait.ie>
2014-10-08 13:50:26 +0100

    Eight commit

--- forExercises.java
+++ forExercises.java
@@ -5,11 +5,13 @@ public class forExercises {
 	final static int NUM_INPUTS = 3;
 	
 	public static void main(String[] args) {
-		forHeaderTesting();
+		/*forHeaderTesting();
 		forSmallestLargest();
 		forHistogram();
 		forAverage();
 		forSequence();
+		forSomeEvenNumbers();*/
+		twelveDaysOfChristmas();
 	}
 	
 	public static void forHeaderTesting() {
@@ -90,6 +92,121 @@ public class forExercises {
 			}
 			else {
 				System.out.print(output);
+			}
+		}
+	}
+	
+	public static void forSomeEvenNumbers() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two even numbers: ");
+		int from = 0 ; int to = 0;
+		from = sc.nextInt();
+		to = sc.nextInt();
+		if ( ((from % 2) != 0) || ((to % 2) != 0)) {
+			System.err.println("InputMismatchException");
+		}
+		else {
+			int sum = 0;		
+			for (int i = from + 1 ; i < to; i ++) {
+				if ((i % 2) == 0) {
+					sum += i;
+				}
+			}
+			System.out.println("Sum of even numbers between " + from + " and " + to + " is " + sum );
+		}
+	}
+	
+	public static void twelveDaysOfChristmas() {
+		Scanner sc = new Scanner(System.in);
+		String day = null;		
+		for (int i = 0 ; i < 12 ; i ++) {
+			switch (i) {
+				case 0:
+					day = "first";
+					break;
+				case 1:
+					day = "second";
+					break;
+				case 2:
+					day = "third";
+					break;
+				case 3:
+					day = "fourth";
+					break;
+				case 4:
+					day = "fifth";
+					break;
+				case 5:
+					day = "sixth";
+					break;
+				case 6:
+					day = "seventh";
+					break;
+				case 7:
+					day = "eight";
+					break;
+				case 8:
+					day = "ninth";
+					break;
+				case 9:
+					day = "tenth";
+					break;
+				case 10:
+					day = "eleventh";
+					break;
+				case 11:
+					day = "twelfth";
+					break;
+			}
+			System.out.println("On the " + day + " day of Christmas,\nMy true love sent to me:");
+			switch (i) {
+				case 0:
+					System.out.println("\tA partridge in a pear tree.\n\n");
+					break;
+				case 1:
+					System.out.println("\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 2:
+					System.out.println("\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 3:
+					System.out.println("\tFour calling birds,\n\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 4:
+					System.out.println("\tFive golden rings,\n\tFour calling birds,\n\tThree French hens,\n\tTwo turtle doves, and" +
+					"\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 5:
+					System.out.println("\tSix geese a-laying,\n\tFive golden rings,\n\tFour calling birds,\n\tThree French hens," +
+					"\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 6:
+					System.out.println("\tSeven swans a-swimming,\n\tSix geese a-laying,\n\tFive golden rings,\n\tFour calling birds," +
+					"\n\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 7:
+					System.out.println("\tEight maids a-milking,\n\tSeven swans a-swimming,\n\tSix geese a-laying," +
+					"\n\tFive golden rings,\n\tFour calling birds,\n\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 8:
+					System.out.println("\tNine ladies dancing,\n\tEight maids a-milking,\n\tSeven swans a-swimming,\n\tSix geese a-laying," +
+					"\n\tFive golden rings,\n\tFour calling birds,\n\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 9:
+					System.out.println("\tTen lords a-leaping,\n\tNine ladies dancing,\n\tEight maids a-milking,\n\tSeven swans a-swimming," +
+					"\n\tSix geese a-laying,\n\tFive golden rings,\n\tFour calling birds,\n\tThree French hens,\n\tTwo turtle doves, and" +
+					"\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 10:
+					System.out.println("\tEleven pipers piping,\n\tTen lords a-leaping,\n\tNine ladies dancing,\n\tEight maids a-milking," +
+					"\n\tSeven swans a-swimming,\n\tSix geese a-laying,\n\tFive golden rings,\n\tFour calling birds,\n\tThree French hens," +
+					"\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
+				case 11:
+					System.out.println("\tTwelve drummers drumming,\n\tEleven pipers piping,\n\tTen lords a-leaping,\n\tNine ladies dancing," +
+					"\n\tEight maids a-milking,\n\tSeven swans a-swimming,\n\tSix geese a-laying,\n\tFive golden rings,\n\tFour calling birds," +
+					"\n\tThree French hens,\n\tTwo turtle doves, and\n\tA partridge in a pear tree.\n\n");
+					break;
 			}
 		}
 	}
