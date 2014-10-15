18ea76335929d142e9f2b63be3c9455fd2959220
unknown <A00226313@W301-A16.studentnet.ait.ie>
2014-10-15 13:44:52 +0100

    Tenth commit

---
+++ PhoneTest.java
@@ -0,0 +1,62 @@
+class Phone {
+
+	private String theNumber;
+	private String theOwnersName;
+	private double thePrice;
+
+	public Phone() {
+		theNumber = "";
+		theOwnersName = "";
+		thePrice = 0.0;
+	}
+
+	public Phone(String aNumber, String aOwnersName, double aPrice) {
+		theNumber = aNumber;
+		theOwnersName = aOwnersName;
+		thePrice = aPrice;
+	}
+
+	@Override
+	public String toString() { //Overriding default toString Method
+		return "The phone number is " + theNumber + ". " + theOwnersName + " owns" +
+		" this phone. The phone cost €" + thePrice;
+	}
+	
+	public String getNumber() {
+		return theNumber;
+	}
+
+	public String getOwnersName() {
+		return theOwnersName;
+	}
+
+	public double getPrice() {
+		return thePrice;
+	}
+
+	public void setNumber(String aNumber) {
+		this.theNumber = aNumber;
+	}
+
+	public void setOwnersName(String aOwnersName) {
+		this.theOwnersName = aOwnersName;
+	}
+
+public void setPrice(double aPrice) {
+		this.thePrice = aPrice;
+	}
+}
+
+public class PhoneTest {
+	public static void main(String[] args) {
+		Phone p = new Phone();
+		Phone p2 = new Phone("12345", "Neil", 0.5);
+		p.setPrice(0.2);
+		System.out.println(p.getPrice());
+		System.out.println(p2.getPrice());
+		System.out.println(p2.getOwnersName());
+		System.out.println(p2.getNumber());		
+		System.out.println(p);
+		System.out.println(p2);
+	}
+}
---
+++ VolumeExample.java
@@ -0,0 +1,44 @@
+class Volume {
+	private int theLength;
+	private int theWidth;
+	private int theHeight;
+
+	public Volume() {
+		theLength = 0;
+		theWidth = 0;
+		theHeight = 0;
+	}
+
+	public Volume(int aLength, int aWidth, int aHeight) {
+		theLength = aLength;
+		theWidth = aWidth;
+		theHeight = aHeight;
+	}
+
+	public int getVolume() {
+		return theLength * theWidth * theHeight;
+	}
+
+	public void setVolumeint(int aLength, int aWidth, int aHeight) {
+		this.theLength = aLength;
+		this.theWidth = aWidth;
+		this.theHeight = aHeight;
+	}
+
+	public int calcVolume() {
+		int volume = theLength * theWidth * theHeight;
+		return volume;
+	}
+
+	@Override
+	public String toString() {
+		return "The volume is " + calcVolume() + " m^3";
+	}
+}
+
+public class VolumeExample {
+	public static void main(String[] args) {
+		Volume v = new Volume(3,4,5);
+		System.out.println(v);
+	}
+}
